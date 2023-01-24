20126bd6f39a591e9ac7c030eae891065e115138
alinu1 <uwu1343@gmail.com>
2023-01-24 23:50:22 +0600

    add models

--- home/models.py
+++ home/models.py
@@ -1,5 +1,44 @@
 from django.db import models
-
+import uuid 
 # Create your models here.
+class BaseModel(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
 
+    class Meta:
+        abstract = True
 
+class Categories(BaseModel):
+    category_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Question(BaseModel):
+    category = models.ForeignKey(Categories, related_name='category', on_delete=models.CASCADE)
+    question = models.TextField()
+    marks = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+class Answer(BaseModel):
+    question = models.ForeignKey(Question,related_name='question_answer' , on_delete=models.CASCADE)
+    answer = models.TextField()
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
